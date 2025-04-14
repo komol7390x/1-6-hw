17e9afd13f064f30fdceddb53f42acc387340c1e
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-14 23:21:01 +0500

    up

---
+++ js/qoshimcha.js
@@ -0,0 +1,12 @@
+// Home work
+// task-1
+// function calc(x, y) {
+//     let sum=0
+//     for (let i = 1; i <= x; i++){
+//         sum+=i**y
+//     }
+//     return sum
+// }
+// console.log(calc(3,4));
+
+// task-2
