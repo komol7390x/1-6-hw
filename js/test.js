5cff8b4495fedd669a3878d0b24061dcc1be9b49
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-14 23:47:28 +0500

    up

--- js/test.js
+++ js/test.js
@@ -1,25 +1,11 @@
 // task-2
-// function isPrime(num) {
-//     if (num < 2)
-//         return false;
-//     for (let i = 2; i <= Math.sqrt(num); i++) {
-//         if (num % i === 0) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-// function calc(n, m) {
-//     let sum = 1
-//     for (let i = n; i <= m; i++) {
-//         let a = isPrime(i)
-//         if (a == true) {
-//             sum += i
-//         }
-//     }
-//     return sum
-// }
-// console.log(("Natija: " + calc(1, 20)));
-
-
-
+function isEqual(str1="", str2) {
+    str1 = str1.split("")
+    for (let i = 0; i < str1.length; i++){
+        if (str1[i].includes(str2)) {
+            return false
+        }
+    }
+    return true
+}
+console.log(isEqual("abd", "bac"));
