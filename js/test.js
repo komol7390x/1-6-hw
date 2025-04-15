96d7d106d52f2fd22be2f82e74ea9ab202bab4eb
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-15 10:04:33 +0500

    up

--- js/test.js
+++ js/test.js
@@ -1,16 +1,19 @@
 // task-5
-// function func1(n) {
-//     n = n.sort((a,b)=>a-b)
-//     console.log(n);
-//     min = n[0]
-//     return (m) => {
-//         let len = n.length
-//         for (let i = 1; i < len; i++){
-//             if (n[i] > n[i - 1] && n[i] < m) {
-//                 min=n[i]
-//             }
-//         }
-//         console.log("Eng yaqin son: "+min);
-//     };
-// }
-// func1([16, 8, 2, 1, 5, 9, 3])(10)
+function func1(n) {
+    let len = n.length
+    const x = [...n].sort((a, b) => a - b)
+    const y = n.sort((a, b) => b - a)
+    console.log(x);
+    console.log(y);
+    min = x[0];
+    max = y[0]
+    return (m) => {
+        for (let i = 1; i < len; i++){
+            if (n[i] > n[i - 1] && n[i] < m) {
+                min=n[i]
+            }
+        }
+        console.log("Eng yaqin son: "+min);
+    };
+}
+func1([16, 8, 2, 1, 5, 9, 3]),(10)
