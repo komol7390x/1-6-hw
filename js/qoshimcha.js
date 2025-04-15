dc13854f18f380da9f4258d2117dc1177c8e55b4
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-15 09:46:33 +0500

    up

--- js/qoshimcha.js
+++ js/qoshimcha.js
@@ -59,3 +59,20 @@
 //     return fibonacci(n - 1) + fibonacci(n - 2);
 // }
 // console.log(fibonacci(10));
+
+// task-5
+// function func1(n) {
+//     n = n.sort((a,b)=>a-b)
+//     console.log(n);
+//     min = n[0]
+//     return (m) => {
+//         let len = n.length
+//         for (let i = 1; i < len; i++){
+//             if (n[i] > n[i - 1] && n[i] < m) {
+//                 min=n[i]
+//             }
+//         }
+//         console.log("Eng yaqin son: "+min);
+//     };
+// }
+// func1([16, 8, 2, 1, 5, 9, 3])(10)
--- js/test.js
+++ js/test.js
@@ -1,10 +1,16 @@
-// task-4
-// function fibonacci(n) {
-//     if (n < 0) {
-//         return "Musbat butun son kiriting!";
-//     }
-//     if (n === 0) return 0;
-//     if (n === 1) return 1;
-//     return fibonacci(n - 1) + fibonacci(n - 2);
+// task-5
+// function func1(n) {
+//     n = n.sort((a,b)=>a-b)
+//     console.log(n);
+//     min = n[0]
+//     return (m) => {
+//         let len = n.length
+//         for (let i = 1; i < len; i++){
+//             if (n[i] > n[i - 1] && n[i] < m) {
+//                 min=n[i]
+//             }
+//         }
+//         console.log("Eng yaqin son: "+min);
+//     };
 // }
-// console.log(fibonacci(10));
+// func1([16, 8, 2, 1, 5, 9, 3])(10)
